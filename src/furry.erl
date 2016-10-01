698379d8a6c773e5cbd3f0adc9c2e013a091a471
Alexey Nikitin <nikitin@bookmate.com>
2016-10-01 18:58:57 +0300

    fresh attempt from scratch

--- src/furry.erl
+++ src/furry.erl
@@ -1,96 +1,56 @@
 -module(furry).
 
-%% API exports
 -export([format/1]).
 
-%%====================================================================
-%% API functions
-%%====================================================================
+format(Map) ->
+  format(Map, undefined).
 
-format(SqlMap) ->
-  format(SqlMap, undefined, []).
+format(#{select := SelectClause} = Map, _) ->
+  List = lists:map(fun to_binary/1, SelectClause),
+  Acc = [<<"SELECT ">>, join_with_comma(List)],
+  format(maps:remove(select, Map), Acc);
+format(#{from := FromClause} = Map, Acc0) ->
+  List = lists:map(fun to_binary/1, FromClause),
+  Acc = [Acc0, <<" FROM ">>, join_with_comma(List)],
+  format(maps:remove(from, Map), Acc);
+format(#{where := WhereClause} = Map, Acc0) ->
+  Where = build_conditions(WhereClause),
+  Acc = [Acc0, <<" WHERE ">>, Where],
+  format(maps:remove(where, Map), Acc);
+format(#{}, Acc) ->
+  iolist_to_binary(Acc).
 
-%%====================================================================
-%% Internal functions
-%%====================================================================
+to_binary(Atom) when is_atom(Atom) ->
+  atom_to_binary(Atom, utf8);
+to_binary(Int) when is_number(Int) ->
+  integer_to_binary(Int);
+to_binary(List) when is_list(List) ->
+  iolist_to_binary(List);
+to_binary(Bin) when is_binary(Bin) ->
+  Bin;
+to_binary(_) ->
+  <<>>.
 
-format(#{select := Select} = SqlMap, _, Bindings) ->
-  Sql = space_join(["SELECT", select_clause(Select)]),
-  format(maps:remove(select, SqlMap), Sql, Bindings);
-format(#{from := From} = SqlMap, Sql0, Bindings) ->
-  Sql = space_join([Sql0, "FROM", from_clause(From)]),
-  format(maps:remove(from, SqlMap), Sql, Bindings);
-format(#{where := Where} = SqlMap, Sql0, Bindings) ->
-  Sql = space_join([Sql0, "WHERE", where_clause(Where)]),
-  format(maps:remove(where, SqlMap), Sql, Bindings);
-format(#{}, Sql, Bindings) ->
-  [Sql | Bindings].
+join_with_comma(List) ->
+  join_binareis(List, ", ").
 
-select_clause(Select) ->
-  comma_join(Select).
+join_binareis([], _Sep) ->
+  [];
+join_binareis([H|T], Sep) ->
+  iolist_to_binary([H, [[Sep, X] || X <- T]]).
 
-from_clause(From) ->
-  comma_join(lists:map(fun handle_from/1, From)).
+wrap_with_parentheses(Bin) ->
+  iolist_to_binary(["(", Bin, ")"]).
 
-where_clause(Where) ->
-  handle_operation(Where).
-
-handle_from({TableName, Alias}) ->
-  space_join([
-    to_string(TableName),
-    "AS",
-    quotes_wrap(Alias)
-  ]);
-handle_from(TableName) ->
-  to_string(TableName).
-
-handle_operation([Operation | Args]) when is_atom(Operation) ->
-  paren_wrap(string:join(
-    lists:map(fun handle_operation/1, Args),
-    space_wrap(operation(Operation))
-  ));
-handle_operation(Arg) -> Arg.
-
-operation(eq) -> "=";
-operation(ne) -> "<>";
-operation(gt) -> ">";
-operation(lt) -> "<";
-operation(ge) -> ">=";
-operation(le) -> "<=";
-operation(is) -> "IS";
-operation(is_not) -> "IS NOT".
-
-to_string(Bin)  when is_binary(Bin) -> binary_to_list(Bin);
-to_string(Atom) when is_atom(Atom)  -> atom_to_list(Atom);
-to_string(List) when is_list(List)  -> List.
-
-map_string(L) -> lists:map(fun to_string/1, L).
-
-comma_join(S) -> string:join(map_string(S), ", ").
-
-space_join(S) -> string:join(map_string(S), " ").
-
-parameterize() -> not_implemented.
-
-paren_wrap(X) ->
-  comp(["(", X, ")"], [
-    fun erlang:iolist_to_binary/1,
-    fun to_string/1
-  ]).
-
-quotes_wrap(X) ->
-  comp(["\"", X, "\""], [
-    fun erlang:iolist_to_binary/1,
-    fun to_string/1
-  ]).
-
-space_wrap(X) ->
-  comp([" ", X, " "], [
-    fun erlang:iolist_to_binary/1,
-    fun to_string/1
-  ]).
-
-comp(Initial, Funs) ->
-  lists:foldl(fun(Fun, AccIn) ->
-    Fun(AccIn)
-  end, Initial, Funs).
+build_conditions(['and'|Conditions]) ->
+  ConditionBinaries = [
+    wrap_with_parentheses(build_conditions(C)) ||
+    C <- Conditions
+  ],
+  join_binareis(ConditionBinaries, " AND ");
+build_conditions([eq|Args]) ->
+  Binaries = [to_binary(X) || X <- Args],
+  join_binareis(Binaries, " = ");
+build_conditions([gt|Args]) ->
+  Binaries = [to_binary(X) || X <- Args],
+  join_binareis(Binaries, " > ").
--- test/furry_tests.erl
+++ test/furry_tests.erl
@@ -1,34 +1,35 @@
 -module(furry_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-format_test() ->
-  SqlMap = #{
-    select => [a, b, c],
-    from => [foo],
-    where => [eq, "f.a", "baz"]
-  },
-  Query = furry:format(SqlMap),
-  ?assertEqual(["SELECT a, b, c FROM foo WHERE (f.a = $1)", "baz"], Query).
+simple_select_test() ->
+  Query = furry:format(#{
+    select => [1]
+  }),
+  ?assertEqual(<<"SELECT 1">>, Query).
 
-format_big_query_test() ->
-  SqlMap = #{
-    select => ["f.*", "b.baz", "c.quux", {"b.bla", "bla-bla"}],
-    modifiers => [distinct],
-    from => [{foo, f}, {baz, b}],
-    join => [draq, [eq, "f.b" "draq.x"]],
-    left_join => [{clod, c}, [eq, "f.a", "c.d"]],
-    right_join => [bock, [eq, "bock.z" "c.e"]],
-    where => ['or',
-      ['and', [eq, "f.a", "bort"], [ne, "b.baz", "param1"]],
-      [lt, 1, 2, 3],
-      [in, "f.e", [1, 2, 3]],
-      [between, "f.e", 10, 20]
-    ],
-    group_by => ["f.a"],
-    having => [le, [0, "f.e"]],
-    order_by => [{"b.baz", desc}, "c.quux", {"f.a", nulls_first}],
-    limit => 50,
-    offset => 10
-  },
-  QueryExpected = "SELECT DISTINCT f.*, b.baz, c.quux, b.bla AS \"bla-bla\" FROM foo AS f, baz AS b INNER JOIN draq ON f.b = draq.x LEFT JOIN clod AS c ON f.a = c.d RIGHT JOIN bock ON bock.z = c.e WHERE ((f.a = ? AND b.baz <> ?) OR (1 < 2 AND 2 < 3) OR (f.e IN (1, ?, 3)) OR f.e BETWEEN 10 AND 20) GROUP BY f.a HAVING 0 < f.e ORDER BY b.baz DESC, c.quux, f.a NULLS FIRST LIMIT 50 OFFSET 10",
-  ?assertEqual([QueryExpected | ["bort", "gabba", 2]], furry:format(SqlMap)).
+from_test() ->
+  Query = furry:format(#{
+    select => [foo, bar],
+    from => [table1]
+  }),
+  ?assertEqual(<<"SELECT foo, bar FROM table1">>, Query).
+
+where_test() ->
+  Query = furry:format(#{
+    select => ["*"],
+    from => [tab1],
+    where => [eq, id, 17]
+  }),
+  ?assertEqual(<<"SELECT * FROM tab1 WHERE id = 17">>, Query).
+
+complex_where_test() ->
+  Query = furry:format(#{
+    select => ["*"],
+    from => [t1],
+    where => [
+      'and',
+      [eq, tel, 17434],
+      [gt, id, 21]
+    ]
+  }),
+  ?assertEqual(<<"SELECT * FROM t1 WHERE (tel = 17434) AND (id > 21)">>, Query).
